efa658228adbcc4a1595e5466830a49f687c5666
Daniel <nqthai611@gmail.gmailcom>
2026-08-12 15:12:05 +0700

    feat: docker and redis

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InfrastructureHealthModule } from './infrastructure/infrastructure-health.module';
 
 @Module({
-  imports: [],
+  imports: [InfrastructureHealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
